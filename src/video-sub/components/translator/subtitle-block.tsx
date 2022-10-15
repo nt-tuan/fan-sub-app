f9524a270b75143485cd275e7f8b9b0391d3e93d
thuannc <thuannc>
2022-10-15 16:38:01 +0700

    feat: onunfocus onsave

--- src/video-sub/components/translator/subtitle-block.tsx
+++ src/video-sub/components/translator/subtitle-block.tsx
@@ -2,9 +2,12 @@ import { Button, Divider } from "antd";
 import classnames from "classnames";
 import debounce from "lodash.debounce";
 import React from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
 
 import useOutsideClick from "@/hooks/use-outside-click";
 import { formatTime } from "@/utils/format";
+import { SaveFilled } from "@ant-design/icons";
 
 import { useSubtitleEditor } from "../../provider";
 import styles from "./styles.module.scss";
@@ -31,6 +34,14 @@ export const SubtitleBlock = ({
 
   const subtitle = editingSubtitles?.[index];
   const refSubtitle = refSubtitles?.[index];
+  const isActive = subtitle && isActiveSubtitle(subtitle);
+
+  const [isSaved, setSaved] = React.useState<boolean>(false);
+  const [isDirty, setDirty] = React.useState<boolean>(false);
+
+  const ref = React.useRef<HTMLTextAreaElement>(null);
+  useOutsideClick(ref, unfocus);
+
   const debounceChangeSubtitle = React.useMemo(
     () => debounce(changeSubtitleText, 300),
     [changeSubtitleText]
@@ -38,38 +49,51 @@ export const SubtitleBlock = ({
   const handleChange = React.useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       debounceChangeSubtitle(index, e.target.value);
+      setDirty(true);
     },
     [index, debounceChangeSubtitle]
   );
-  const handleFocus = () => {
+
+  const handleSave = useCallback(() => {
+    saveSubtitle(index);
+    setDirty(false);
+    setSaved(true);
+  }, [index, saveSubtitle]);
+
+  const onFocus = () => {
     if (!subtitle) return;
     focus(subtitle);
   };
 
-  const ref = React.useRef<HTMLTextAreaElement>(null);
-  useOutsideClick(ref, unfocus);
-
-  const handleCancel = () => {
+  const onCancel = () => {
     cancelSubtitle(index);
+    setDirty(false);
     if (ref.current && subtitle?.text != null)
       ref.current.value = getDefaultSubtitleText(index);
   };
+
+  const onSave = () => handleSave();
 
   // for replace word modal
   React.useEffect(() => {
     if (ref.current && isModalOpen) ref.current.value = subtitle?.text ?? "";
   }, [ref, subtitle?.text, isModalOpen]);
 
-  const isActive = subtitle && isActiveSubtitle(subtitle);
+  // unfocus handler
+  useEffect(() => {
+    if (isDirty && !isActive) handleSave();
+  }, [isActive, isDirty, handleSave]);
+
   if (subtitle == null)
     return <div style={style} className={styles.segment_container}></div>;
+
   return (
     <div
       style={style}
       className={classnames(styles.subtitle_block, {
         [styles.subtitle_segment_active]: isActive,
       })}
-      onClick={handleFocus}
+      onClick={onFocus}
     >
       <div className={styles.subtitle_time_segment}>
         {formatTime(subtitle.from)} - {formatTime(subtitle.to)}
@@ -81,7 +105,6 @@ export const SubtitleBlock = ({
         ref={ref}
         className={styles.subtitle_input}
         defaultValue={subtitle.text}
-        // value={subtitle.text}
         onChange={handleChange}
         rows={3}
       />
@@ -89,17 +112,26 @@ export const SubtitleBlock = ({
         <Button
           className={styles.subtitle_segment_action_item}
           type="text"
-          onClick={handleCancel}
+          onClick={onCancel}
         >
           Cancel
         </Button>
         <Divider type="vertical" />
         <Button
-          className={styles.subtitle_segment_action_item}
-          onClick={() => saveSubtitle(index)}
+          className={classnames(
+            styles.subtitle_segment_action_item,
+            styles[
+              isDirty
+                ? "subtitle_segment_action_item_unsaved"
+                : isSaved
+                ? "subtitle_segment_action_item_saved"
+                : "subtitle_segment_action_item_default"
+            ]
+          )}
+          onClick={onSave}
           type="text"
         >
-          Save
+          {isDirty || !isSaved ? "Save" : "Saved"}
         </Button>
       </div>
     </div>
